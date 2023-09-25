90e455a99581338862bb3f7610ac5f27bda4a4ab
RenegateSurvaival <140224544+RenegateSurvaival@users.noreply.github.com>
2023-09-25 12:25:18 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+window.addEventListener("load", function() {
+	const preloader = document.querySelector(".preloader");
+	preloader.classList.add('animatePsOpas');
+	setTimeout(() => {
+		preloader.style.display = 'none';
+	}, 1350);
+});
+
+
+particlesJS.load('particles-js', './config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js', '../config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
